25286f1c5ac5bb409df21ed5e219691f3c2f9a03
Matt Mandersheid <mmandersheid@gmail.com>
2024-01-22 19:24:33 -0500

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Great Dawn',
+    site_url: 'https://great-north.vercel.app/',
+    feed_url: 'https://great-north.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
